a6fce09303e56a425f4b1572022dd352827936b3
Marcin Zadka <gamboox@gmail.com>
2019-12-02 15:13:50 +0100

    Add model tests

--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Area, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new }
+
+  it "is valid with valid geometry" do
+    factory = RGeo::Cartesian.preferred_factory
+    p00 = factory.point(0, 0)
+    p20 = factory.point(2, 0)
+    p11 = factory.point(1, 1)
+    ring = factory.linear_ring([p00, p11, p20, p00])
+    polygon = factory.polygon(ring)
+    subject.geom = polygon
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a geometry" do
+    expect(subject).to_not be_valid
+  end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new }
+
+  it "is valid with valid name and geometry" do
+    factory = RGeo::Cartesian.preferred_factory
+    point = factory.point(0, 0)
+    subject.geom = point
+    subject.name = 'Name'
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a geometry" do
+    subject.name = 'Name'
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a name" do
+    factory = RGeo::Cartesian.preferred_factory
+    point = factory.point(0, 0)
+    subject.geom = point
+    expect(subject).to_not be_valid
+  end
 end
